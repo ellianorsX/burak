6face476d51917512a9e0c87f1f2b3df746fe08e
ellianorsX <marjonathe98@icloud.com>
2026-05-25 15:50:40 +0900

    feat: MI-TASK 13M

--- train.py
+++ train.py
@@ -8,4 +8,22 @@ def gaplarniReverse(text):
 
 print(gaplarniReverse("I am Developer and Engineer"))
 
+
+print("==========MI-TASK 13M ============")
+
+def getSquareNumbers(numbers):
+    result = []
     
+    for raqam in numbers:
+        object = {
+            "number": raqam,
+            "square": raqam * raqam 
+        }
+        
+        result.append(object)
+        
+        
+    return result
+
+
+print(getSquareNumbers([1, 2, 3, 4, 5]));
