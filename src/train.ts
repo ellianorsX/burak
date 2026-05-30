6ec9448b935e7ff6a71d7eba85e1842645e08494
ellianorsX <marjonathe98@icloud.com>
2026-05-30 14:01:05 +0900

    feat: MI-TASK 14N in TS

--- src/train.ts
+++ src/train.ts
@@ -2,17 +2,32 @@
 
 //Resultni hosil qiladigan object tipi
 
-interface SquareResult {
-  number: number;
-  square: number;
+// interface SquareResult {
+//   number: number;
+//   square: number;
+// }
+
+// function getSquareNumbers(numbers: number[]): SquareResult[] {
+//   //map = har bir elementga funcsion qo'llaydi!
+//   return numbers.map((num) => ({
+//     number: num,
+//     square: num * num,
+//   }));
+// }
+
+// console.log(getSquareNumbers([3, 4, 6, 8]));
+
+//print("==========MI-TASK 14N ============")
+
+function palindromCheck(srt: string): boolean {
+  //stringni reverse qilish orqali haqiiqy matn bilan taqqoslaymaiz
+
+  const cleaned: string = srt.toLowerCase();
+  const reversed: string = cleaned.split("").reverse().join("");
+
+  return cleaned === reversed;
 }
 
-function getSquareNumbers(numbers: number[]): SquareResult[] {
-  //map = har bir elementga funcsion qo'llaydi!
-  return numbers.map((num) => ({
-    number: num,
-    square: num * num,
-  }));
-}
-
-console.log(getSquareNumbers([3, 4, 6, 8]));
+console.log(palindromCheck("pop"));
+console.log(palindromCheck("jazz"));
+console.log(palindromCheck("rock"));
