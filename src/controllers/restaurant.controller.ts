08f47a9fbcdc57e58e5e43a09e56df0cbf0c30df
ellianorsX <marjonathe98@icloud.com>
2026-06-06 16:11:33 +0900

    feat: frontend related css, java and image ceration

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -11,7 +11,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
     //LOGIC
     //SERVICE MODEL
     console.log("goHome");
-    res.send("Home Page");
+    res.render("home");
   } catch (err) {
     console.log("Error, goHome:", err);
   }
@@ -20,7 +20,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 restaurantController.getSignup = (req: Request, res: Response) => {
   try {
     console.log("getSignup");
-    res.send("SignUp Page");
+    res.render("signup");
   } catch (err) {
     console.log("Error, getSignup:", err);
   }
@@ -29,7 +29,7 @@ restaurantController.getSignup = (req: Request, res: Response) => {
 restaurantController.getLogin = (req: Request, res: Response) => {
   try {
     console.log("getLogin");
-    res.send("Login  Page");
+    res.render("login");
   } catch (err) {
     console.log("Error, getLogin:", err);
   }
@@ -44,7 +44,7 @@ restaurantController.processSignup = async (req: Request, res: Response) => {
 
     const result = await memberService.processSignup(newMember);
 
-    //TODO: SESSIOONS
+    //TODO: SESSIOONS AUTENTICATION
 
     res.send(result);
   } catch (err) {
@@ -60,6 +60,8 @@ restaurantController.processLogin = async (req: Request, res: Response) => {
     const input: LoginInput = req.body;
 
     const result = await memberService.processLogin(input);
+
+    // TODO:
 
     res.send(result);
   } catch (err) {
