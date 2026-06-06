48150a01d902d3fe0136d5c0f1036bd47c90fd4e
ellianorsX <marjonathe98@icloud.com>
2026-06-06 17:06:41 +0900

    feat: MI-TASK 17Q

--- train.py
+++ train.py
@@ -1,82 +1,96 @@
-def gaplarniReverse(text):
-    words = text.split(" ")
+# def gaplarniReverse(text):
+#     words = text.split(" ")
     
-    reversed_words = [word[::-1] for word in words]
+#     reversed_words = [word[::-1] for word in words]
     
-    return " ".join(reversed_words)
+#     return " ".join(reversed_words)
 
 
-print(gaplarniReverse("I am Developer and Engineer"))
+# print(gaplarniReverse("I am Developer and Engineer"))
 
 
-print("==========MI-TASK 13M ============")
+# print("==========MI-TASK 13M ============")
 
-def getSquareNumbers(numbers):
-    result = []
+# def getSquareNumbers(numbers):
+#     result = []
     
-    for raqam in numbers:
-        object = {
-            "number": raqam,
-            "square": raqam * raqam 
-        }
+#     for raqam in numbers:
+#         object = {
+#             "number": raqam,
+#             "square": raqam * raqam 
+#         }
         
-        result.append(object)
+#         result.append(object)
         
         
-    return result
+#     return result
 
 
-print(getSquareNumbers([1, 2, 3, 4, 5]));
+# print(getSquareNumbers([1, 2, 3, 4, 5]));
 
 
 
 
-print("==========MI-TASK 14N ============")
+# print("==========MI-TASK 14N ============")
 
-def palindrom_check(string):
-    string = string.lower()
+# def palindrom_check(string):
+#     string = string.lower()
     
-    reversed_string = string[::-1]
+#     reversed_string = string[::-1]
     
-    return string ==reversed_string
+#     return string ==reversed_string
 
-print(palindrom_check("python"));
-print(palindrom_check("lool"));
+# print(palindrom_check("python"));
+# print(palindrom_check("lool"));
     
     
-print("==========MI-TASK 15O ============")
+# print("==========MI-TASK 15O ============")
 
-def calculateSumOfNumber(arr):
-    total = 0
+# def calculateSumOfNumber(arr):
+#     total = 0
     
-    for item in arr:
-        if type(item) == int:
-            total = total + item
+#     for item in arr:
+#         if type(item) == int:
+#             total = total + item
             
-        elif type(item) == float:
-            total = total + item
+#         elif type(item) == float:
+#             total = total + item
             
             
-        else:
-            pass
+#         else:
+#             pass
         
         
-    return total
+#     return total
 
-arr = [8, "99", '11', 1]
+# arr = [8, "99", '11', 1]
 
-print(calculateSumOfNumber(arr))
+# print(calculateSumOfNumber(arr))
 
 
 
-print("==========MI-TASK 16P ============")
+# print("==========MI-TASK 16P ============")
 
-def objectToArray(obj):
-    result = []
+# def objectToArray(obj):
+#     result = []
     
-    for key in obj:
-        result.append([key, obj[key]])
+#     for key in obj:
+#         result.append([key, obj[key]])
         
-    return result 
+#     return result 
 
-print(objectToArray({"a": 8, "f": 99, "u": 23}));   
+# print(objectToArray({"a": 8, "f": 99, "u": 23}));   
+
+
+print("==========MI-TASK 17Q ============")
+
+def hasProperty(obj: dict, prop: str) -> bool:
+    if prop in obj:
+        return True
+    else:
+        return False
+    
+    
+print(hasProperty({"job": "AI AGENT"}, "job"))   
+print(hasProperty({"job": "AI AGENT"}, "hobby")) 
+print(hasProperty({"phone": "Iphone"}, "earphone"))
