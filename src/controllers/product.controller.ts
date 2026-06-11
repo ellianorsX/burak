b3af4bd57bef15d2de90b9185c16cf09f9bfdf24
ellianorsX <marjonathe98@icloud.com>
2026-06-11 12:56:37 +0900

    feat: locals variable integration

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -10,6 +10,7 @@ const productController: T = {};
 productController.getAllProducts = async (req: Request, res: Response) => {
   try {
     console.log("getAllProducts");
+
     res.render("products");
   } catch (err) {
     console.log("Error, getAllProducts:", err);
