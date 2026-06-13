4580b5b846d988d188d2a77cb3b7c773b9efcc65
ellianorsX <marjonathe98@icloud.com>
2026-06-13 13:33:29 +0900

    fix: getAllProducts api and passing data into ejs

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -13,7 +13,10 @@ productController.getAllProducts = async (req: Request, res: Response) => {
   try {
     console.log("getAllProducts");
 
-    res.render("products");
+    const data = await productService.getAllProducts();
+    console.log("data:", data);
+
+    res.render("products", { products: data });
   } catch (err) {
     console.log("Error, getAllProducts:", err);
 
