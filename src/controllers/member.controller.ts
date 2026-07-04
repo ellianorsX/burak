2b9e94e3ea98f97ba99508393d91974689f786ae
ellianorsX <marjonathe98@icloud.com>
2026-07-04 16:01:25 +0900

    fix: modify getMemberDetail bussiness logic

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -73,6 +73,26 @@ memberController.logout = (req: ExtendedRequest, res: Response) => {
   }
 };
 
+memberController.getMemberDetail = async (
+  req: ExtendedRequest,
+  res: Response,
+) => {
+  try {
+    console.log("getMemberDetail");
+
+    const result = await memberService.getMemberDetail(req.member);
+
+    res.status(HttpCode.OK).json({ member: result });
+
+    res.status(HttpCode.OK).json(result);
+  } catch (err) {
+    console.log("Error, getMemberDetail:", err);
+
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
 // FAQAT LOGIN BO'LGAN USER NEXT BO'LADI
 memberController.verifyAuth = async (
   req: ExtendedRequest,
